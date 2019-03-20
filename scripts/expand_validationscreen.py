913e63ab4f4627d1ccdc24898820518158057ce9
Dominik Lindner <d.lindner@dundee.ac.uk>
2019-03-20 11:30:33 +0000

    flake8

--- scripts/expand_validationscreen.py
+++ scripts/expand_validationscreen.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 '''
 The original annotations for the validation screen
-contain several wells and two plates per row.
-This script simply splits these rows into one plate 
-/ one well per row, as expected as input for the 
+contain several wells and two plates per row
+This script simply splits these rows into one plate
+/ one well per row, as expected as input for the
 metadata plugin.
 '''
 
@@ -14,18 +14,18 @@ out_file = "../screen3/idr0056_screen3-annotation.csv"
 df = pd.read_csv(in_file)
 header = df.columns.values.tolist()
 
-out = pd.DataFrame(columns = header)
+out = pd.DataFrame(columns=header)
 
 for index, row in df.iterrows():
-  for well in row['Well'].split("-"):
-    if well:
-      newrow1 = row.copy()
-      newrow2 = row.copy()
-      newrow1['Plate'] = 'plate1'
-      newrow2['Plate'] = 'plate2'
-      newrow1['Well'] = well
-      newrow2['Well'] = well
-      out = out.append(newrow1, ignore_index=True)
-      out = out.append(newrow2, ignore_index=True)
+    for well in row['Well'].split("-"):
+        if well:
+            newrow1 = row.copy()
+            newrow2 = row.copy()
+            newrow1['Plate'] = 'plate1'
+            newrow2['Plate'] = 'plate2'
+            newrow1['Well'] = well
+            newrow2['Well'] = well
+            out = out.append(newrow1, ignore_index=True)
+            out = out.append(newrow2, ignore_index=True)
 
 out.to_csv(out_file, index=False)
